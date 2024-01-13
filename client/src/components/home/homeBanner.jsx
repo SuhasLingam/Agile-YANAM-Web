c55498494a0393d658b17faca24a7599b0c55629
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2024-01-13 18:45:42 +0530

    ...

--- client/src/components/home/homeBanner.jsx
+++ client/src/components/home/homeBanner.jsx
@@ -4,11 +4,11 @@ import { FaArrowRight } from "react-icons/fa";
 const homeBanner = () => {
   return (
     <div className="font-poppins md:pt-[180px] flex flex-col items-center justify-between w-full h-screen">
-      <div className="md:space-y-3 space-y-2 font-extrabold md:font-bold md:mt-[0px] mt-[200px] ">
+      <div className="md:space-y-3 space-y-2 flex flex-col items-center justify-center font-extrabold md:font-bold md:mt-[0px] mt-[200px] ">
         <p className="md:text-5xl flex items-center justify-center text-lg text-justify text-black">
           Crafting Your Digital Destiny with
         </p>
-        <p className="md:text-5xl text-wrap flex items-center justify-center text-lg text-black">
+        <p className="md:text-5xl flex items-center justify-center text-lg text-justify text-black break-all">
           Enhancing your ability with customised solutions
         </p>
       </div>
