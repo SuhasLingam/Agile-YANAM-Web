b551c4e21a996177e5d4a2787bf6d243458c48f1
SuhasLingam <sl7215@srmist.edu.in>
2024-02-04 18:21:49 +0530

    Added Website , Video Edit , UiUx images and links

--- client/data.js
+++ client/data.js
@@ -1,7 +1,41 @@
+import UI1 from './src/assets/1UI.png';
+import UI2 from './src/assets/2UI.png';
+import UI3 from './src/assets/3UI.png';
+import web1 from "./src/assets/web1.png";
+import Cart from './src/assets/cart.webp';
+import vid1 from "./src/assets/vid1.png"
+import vid2 from "./src/assets/vid2.png"
+import vid3 from "./src/assets/vid3.png"
+import vid4 from "./src/assets/vid4.png"
+import vid5 from "./src/assets/vid5.png"
+import vid6 from "./src/assets/vid6.png"
+import vid7 from "./src/assets/vid7.png"
+
+
+
+
 const ProjectsList = [
   {
     type: 'UI/UX',
-    title: 'UI/UX Project Example',
+    title: 'UI/UX Project',
+    redirectLink: ["https://www.behance.net/gallery/177500643/UX-UI-PORTFOILO-PET-CARE"],
+    images: UI1,
+    description:
+      "Elevate your digital presence with our expert UI/UX services! From sleek designs to seamless experiences, we're here to redefine user engagement. Ready to transform your digital journey? Let's craft brilliance together.",
+  },
+  {
+    type: 'UI/UX',
+    title: 'UI/UX Project',
+    redirectLink: ["https://www.behance.net/gallery/177500937/DASHBOARD-PORTFOILO-SQUARE-CODE"],
+    images: UI2,
+    description:
+      "Elevate your digital presence with our expert UI/UX services! From sleek designs to seamless experiences, we're here to redefine user engagement. Ready to transform your digital journey? Let's craft brilliance together.",
+  },
+  {
+    type: 'UI/UX',
+    title: 'UI/UX Project',
+    redirectLink: ["https://www.behance.net/gallery/177309263/StreetWare-Website-ui-design"],
+    images: UI3,
     description:
       "Elevate your digital presence with our expert UI/UX services! From sleek designs to seamless experiences, we're here to redefine user engagement. Ready to transform your digital journey? Let's craft brilliance together.",
   },
@@ -13,13 +47,66 @@ const ProjectsList = [
   },
   {
     type: 'Video Editing',
-    title: 'Video Editing Project Example',
+    title: 'Video Editing Project', 
+    images: vid1,
+    redirectLink: "https://drive.google.com/file/d/1OEys4IWigttG-NOqbbCzpUkr4Dcx0Ql_/view?usp=drive_link",
     description:
       "Unleash the power of storytelling with our Video Editing services! From captivating visuals to seamless transitions, we transform raw footage into compelling narratives. Ready to make your content stand out? Let's edit greatness into every frame.",
   },
   {
+    type: 'Video Editing',
+    title: 'Video Editing Project', 
+    images: vid2,
+    redirectLink: "https://drive.google.com/file/d/13NqFqX75mlTF2_gMQn0vfR9cPzRGabz3/view?usp=drive_link",
+    description:
+      "Unleash the power of storytelling with our Video Editing services! From captivating visuals to seamless transitions, we transform raw footage into compelling narratives. Ready to make your content stand out? Let's edit greatness into every frame.",
+  },
+  {
+    type: 'Video Editing',
+    title: 'Video Editing Project', 
+    images: vid3,
+    redirectLink: "https://drive.google.com/file/d/1-vge_I3RQs8YG_XagmgWbya6Jk65tKCY/view?usp=drive_link",
+    description:
+      "Unleash the power of storytelling with our Video Editing services! From captivating visuals to seamless transitions, we transform raw footage into compelling narratives. Ready to make your content stand out? Let's edit greatness into every frame.",
+  },
+  {
+    type: 'Video Editing',
+    title: 'Video Editing Project', 
+    images: vid4,
+    redirectLink: "https://drive.google.com/file/d/1Ur677bDKzXrgRs7LG9nmFMaVmIbMel4w/view?usp=drive_link",
+    description:
+      "Unleash the power of storytelling with our Video Editing services! From captivating visuals to seamless transitions, we transform raw footage into compelling narratives. Ready to make your content stand out? Let's edit greatness into every frame.",
+  },
+  {
+    type: 'Video Editing',
+    title: 'Video Editing Project', 
+    images: vid5,
+    redirectLink: "https://drive.google.com/file/d/11dyXoPKRQ3pnL4CHPTeQfEljayqfKRxH/view?usp=drive_link",
+    description:
+      "Unleash the power of storytelling with our Video Editing services! From captivating visuals to seamless transitions, we transform raw footage into compelling narratives. Ready to make your content stand out? Let's edit greatness into every frame.",
+  },
+  {
+    type: 'Video Editing',
+    title: 'Video Editing Project', 
+    images: vid6,
+    redirectLink: "https://drive.google.com/file/d/1-stz1-uODuwZvpBsta4Rb0nGN3xzVYr3/view?usp=drive_link",
+    description:
+      "Unleash the power of storytelling with our Video Editing services! From captivating visuals to seamless transitions, we transform raw footage into compelling narratives. Ready to make your content stand out? Let's edit greatness into every frame.",
+  },
+  {
+    type: 'Video Editing',
+    title: 'Video Editing Project', 
+    images: vid7,
+    redirectLink: "https://drive.google.com/file/d/1-us9onb1ZmEj0XkWOHeBV8yk5npyBxPR/view?usp=drive_link",
+    description:
+      "Unleash the power of storytelling with our Video Editing services! From captivating visuals to seamless transitions, we transform raw footage into compelling narratives. Ready to make your content stand out? Let's edit greatness into every frame.",
+  },
+
+  {
     type: 'Web Dev',
     title: 'Web Development Project Example',
+    images: web1,
+    redirectLink: ["https://irsbatch2007.in/"],
     description:
       "Empower your online presence with our Web Development expertise! From responsive sites to dynamic web applications, we engineer digital solutions that seamlessly align with your goals. Ready to bring your ideas to life online? Let's code brilliance together.",
   },
@@ -84,28 +171,17 @@ const ProjectsList = [
       "Transform your space into a masterpiece with our Interior Design expertise! From concept to execution, we craft personalized designs that reflect your style and enhance your living or working environment. Ready to redefine your space? Let's design your world together.",
   },
   {
-    type: 'UI/UX',
-    title: 'UI/UX Project Example',
-    description:
-      "Elevate your digital presence with our expert UI/UX services! From sleek designs to seamless experiences, we're here to redefine user engagement. Ready to transform your digital journey? Let's craft brilliance together.",
-  },
-  {
     type: 'Graphic Design',
     title: 'Graphic Design Project Example',
     description:
       "Ignite visual brilliance with our Graphic Design services! From striking logos to eye-catching marketing materials, we craft designs that captivate and resonate. Ready to stand out in the visual landscape? Let's create stunning graphics that leave a lasting impression.",
   },
   {
-    type: 'Video Editing',
-    title: 'Video Editing Project Example',
-    description:
-      "Unleash the power of storytelling with our Video Editing services! From captivating visuals to seamless transitions, we transform raw footage into compelling narratives. Ready to make your content stand out? Let's edit greatness into every frame.",
-  },
-  {
     type: 'Web Dev',
-    title: 'Web Development Project Example',
+    title: 'Coming Soon',
+    images: Cart,
     description:
-      "Empower your online presence with our Web Development expertise! From responsive sites to dynamic web applications, we engineer digital solutions that seamlessly align with your goals. Ready to bring your ideas to life online? Let's code brilliance together.",
+      "Coming Soon",
   },
   {
     type: 'App Dev',
--- client/src/components/Projects/projects.jsx
+++ client/src/components/Projects/projects.jsx
@@ -1,9 +1,8 @@
 import { BsArrowUpRightCircle } from 'react-icons/bs';
-import Cart from '../../assets/cart.webp';
 
 const projects = ({ selectedProject, selectedService }) => {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 ">
       <div className="bg-[#000000]  py-[100px] w-full h-full   flex  flex-col justify-center space-y-12 items-center">
         <p className="md:text-9xl text-6xl text-white font-bold">
           {selectedService}
@@ -13,24 +12,23 @@ const projects = ({ selectedProject, selectedService }) => {
           selectedProject?.map((project, i) => (
             <div
               key={i}
-              className="md:w-[60%] md:flex-row flex-col md:h-[350px] w-[70%] bg-[#FFFFFF]  bg-opacity-20 rounded-3xl text-white flex ">
+              className="md:w-[60%] cursor-pointer border-blue-500 border-4 border-opacity-50 md:flex-row flex-col md:h-[350px] w-[70%] bg-[#FFFFFF]  bg-opacity-20 rounded-3xl text-white flex ">
               <div className="flex flex-col p-11 justify-center items-stretch w-full h-full rounded-3xl">
                 <b className="md:text-5xl text-3xl uppercase">
                   {project.title}
                 </b>
-                <p className="font-semibold text-gray-300">20/20/2024</p>
+                {/* <p className="font-semibold text-gray-300">20/20/2024</p> */}
                 <p>{project.description}</p>
               </div>
               <div className="flex relative justify-center  rounded-3xl items-center md:w-[50%] md:h-full w-full h-[70%]">
-                <a>
+                <a href={project.redirectLink}>
                   <BsArrowUpRightCircle
                     className="absolute z-20 text-white right-11 top-11 "
                     size={50}
                   />
                 </a>
                 <img
-                  src={Cart}
-                  alt=""
+                  src={project.images}
                   className="md:w-full md:h-full w-full h-[280px] rounded-3xl"
                 />
               </div>
